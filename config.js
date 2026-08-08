b814b887e79696c2c847d7abbbc3c2c4551ce7ac
SPMaiMai <spmm0721@gmail.com>
2026-08-07 20:18:23 -0700

    Update MAIONLINE_API_BASE URL for GitHub Pages

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-// Used on GitHub Pages. Set this to your public backend URL (HTTPS).
-// Example: window.MAIONLINE_API_BASE = 'https://maionline-api.onrender.com';
-window.MAIONLINE_API_BASE = '';
+// Used on GitHub Pages. Must be HTTPS (GitHub Pages cannot call plain http).
+window.MAIONLINE_API_BASE = 'https://159.54.171.212.sslip.io';
